c90fdbcd5e5cf8526c18c10b36bc44a4418db0de
Major Tom <reginaldoazevedo27@gmail.com>
2025-09-11 19:26:03 -0300

    Update comfyui-master.sh

--- comfyui-master.sh
+++ comfyui-master.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Clone do ComfyUI (se ainda não existir)
-echo "🔄 Instalando ComfyUI..."
-if [ ! -d "ComfyUI" ]; then
-    git clone https://github.com/comfyanonymous/ComfyUI.git
-fi
 
-cd ComfyUI/custom_nodes
-echo "📥 Instalando ComfyUI-Manager..."
-git clone https://github.com/Comfy-Org/ComfyUI-Manager.git
 
-cd ..
-echo "📦 Instalando dependências do ComfyUI..."
-pip install -r requirements.txt
-pip install triton
-pip install sageattention
-
-echo "🔁 Iniciando ComfyUI..."
-cd /workspace/ComfyUI
-python main.py --port 8188 --host 0.0.0.0
 
 
 
